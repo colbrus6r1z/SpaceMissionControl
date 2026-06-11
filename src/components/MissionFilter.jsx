75598f897264e05842c9d2aa6ef1cfe9caae6044
colbrus6r1z <russell91c@gmail.com>
2026-06-11 10:44:33 -0400

    Complete and working project

---
+++ src/components/MissionFilter.jsx
@@ -0,0 +1,24 @@
+import styles from "./MissionFilter.module.css";
+
+function MissionFilter ({setFilter})
+{
+    const STATUSES = ["All", "Planned", "Active", "Completed"];
+
+    return (
+        <>
+            {
+                STATUSES.map((status, index) => (
+                    <button
+                        key={index}
+                        className={styles.button}
+                        onClick={() => setFilter(status)}
+                    >
+                        {status}
+                    </button>
+                ))
+            }
+        </>
+
+    );
+}
+export default MissionFilter;
